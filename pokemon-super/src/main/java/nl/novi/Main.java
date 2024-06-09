74e98099ffc7e4357ad5b508893d1b22e4cb0d71
Maarten Postma <maarten.postma1000@gmail.com>
2024-06-09 22:11:36 +0200

    Set up the game

--- pokemon-super/src/main/java/nl/novi/Main.java
+++ pokemon-super/src/main/java/nl/novi/Main.java
@@ -2,6 +2,7 @@ package nl.novi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -20,15 +21,47 @@ public class Main {
   public static void main(String[] args) {
 
     // This pokemons need to be put into an array and there I will make a ramdom  object
-    FirePokemon charmender = new FirePokemon(50, 100, 0, 50, 50, 20);
-    WaterPokemon squirtle = new WaterPokemon(50, 100, 0, 50, 50, 20);
-    GrassPokemon bulbasaur = new GrassPokemon(50, 100, 0, 50, 50, 30);
+
+//    FirePokemon charmender = new FirePokemon(50, 100, 0, 50, 50, 20);
+//    WaterPokemon squirtle = new WaterPokemon(50, 100, 0, 50, 50, 20);
+//    GrassPokemon bulbasaur = new GrassPokemon(50, 100, 0, 50, 50, 30);
     ElectricPokemon pikachu = new ElectricPokemon(50, 100, 0, 50, 50, 30);
 
     List<Pokemon> pokemons = new ArrayList<Pokemon>();
-    pokemons.add(charmender);
+    pokemons.add(new FirePokemon(50, 100, 0, 50, 50, 20));
+    pokemons.add(new WaterPokemon(50, 100, 0, 50, 50, 20));
+    pokemons.add(new GrassPokemon(50, 100, 0, 50, 50, 30));
 
     //  scenarios can play out here
+    // When everything is set up I want to be able to base on a scanner system to give a randon challenger
+
+    // Randomize pokemon
+    Random random = new Random();
+    int randomIndex = random.nextInt(pokemons.size());
+    Pokemon randomPokemon = pokemons.get(randomIndex);
+    System.out.println(randomPokemon);
+
+    // the followqing will cast the instance of into the specific type
+
+    // the fight scene could be appearing inside of the if statement
+    if (randomPokemon instanceof FirePokemon) {
+      FirePokemon firePokemon = (FirePokemon) randomPokemon;
+      firePokemon.speaks();
+      System.out.println("Goodluck, your challenger is " + firePokemon.getName());
+
+      // create a while loop to continue a challenge
+    } else if (randomPokemon instanceof WaterPokemon) {
+      WaterPokemon waterPokemon = (WaterPokemon) randomPokemon;
+      waterPokemon.speaks();
+      System.out.println(waterPokemon.getHp());
+      System.out.println("Goodluck, your challenger is " + waterPokemon.getName());
+      ;
+    } else if (randomPokemon instanceof GrassPokemon) {
+      GrassPokemon grassPokemon = (GrassPokemon) randomPokemon;
+      grassPokemon.speaks();
+      System.out.println(grassPokemon.getHp());
+      System.out.println("Goodluck, your challenger is " + grassPokemon.getName());
+    }
 
     // this was a manual test
 //    pikachu.increaseVoltage();
@@ -45,8 +78,6 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
 
   }
-
-  // When everything is set up I want to be able to base on a scanner system to give a ramdon challenger
 
   // this game will just give a couple of choises:
   //  _ start the game
